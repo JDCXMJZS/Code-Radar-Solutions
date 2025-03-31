b9449f5f99257a76ac66f911ac55517fda32e275
Aditya Chandel <chandeladitya2006@gmail.com>
2025-04-01 00:43:11 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,39 +1,65 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-int main() {
-    int n, temp;
+// int main() {
+//     int n, temp;
     
-    // Input size of the array
-    scanf("%d", &n);
+//     // Input size of the array
+//     scanf("%d", &n);
 
-    // Handle edge case where size is less than 2
-    if (n <= 1) {
-        printf("%d", -1);
+//     // Handle edge case where size is less than 2
+//     if (n <= 1) {
+//         printf("%d", -1);
+//         return 0;
+//     }
+
+//     // Declare array
+//     int arr[n];
+
+//     // Input array elements
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     // Bubble sort algorithm
+//     for (int i = 0; i < n - 1; i++) {
+//         for (int j = 0; j < n - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 // Swap elements
+//                 temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+
+//     // Print the second-largest element
+//     printf("%d", arr[n - 2]);
+
+//     return 0;
+// }
+#include<stdio.h>
+int main(){
+    int n,temp;
+    scanf("%d",&n);
+    if(n==1){
+        printf("%d",-1);
         return 0;
     }
-
-    // Declare array
     int arr[n];
-
-    // Input array elements
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0li<n;i++){
+        int x;
+        scanf("%d",&x);
+        arr[i]=x;
     }
-
-    // Bubble sort algorithm
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1]
+                arr[j+1]=temp;
             }
         }
+        printf("%d",arr[n-2]);
     }
-
-    // Print the second-largest element
-    printf("%d", arr[n - 2]);
-
     return 0;
 }
