17b9630951ddc0a7f97c9fdaa04bbb95b79abb01
Aditya Chandel <chandeladitya2006@gmail.com>
2025-04-09 16:35:25 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -15,9 +15,9 @@ int main(){
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
-                temp=arr[j];
+                // temp=arr[j];
                 arr[j]=arr[j+1];
-                arr[j+1]=temp;
+                arr[j+1]=arr[j];
             }
         }
     }
