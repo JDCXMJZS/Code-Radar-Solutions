4db6227211cff5b3e03245cbaf7f9e1b4d561c77
Aditya Chandel <chandeladitya2006@gmail.com>
2025-04-09 16:50:07 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,24 +1,52 @@
+// #include<stdio.h>
+// int main(){
+//     int n,count,order;
+//     scanf("%d",&n);
+//     int array_1[n],updated_array[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&array_1[i]);
+//     }
+//     for(int j=0;j<n;j++){
+//         count=0,order=0;
+//         for(int k=0;k<n;k++){
+//             order+=1;
+//             if(array_1[j]==array_1[k]){
+//                 count+=1;
+//                 updated_array[order-1]=updated_array[j];
+//             }
+//             else{
+//                 updated_array[order-1]=updated_array[j];
+//             }
+//             printf("%d %d\n",updated_array[j],count);
+//             }
+//         }
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    int n,count,order;
+    int n,temp,frequency=0;
     scanf("%d",&n);
-    int array_1[n],updated_array[n];
+    int array_1[n];
     for(int i=0;i<n;i++){
         scanf("%d",&array_1[i]);
     }
-    for(int j=0;j<n;j++){
-        count=0,order=0;
-        for(int k=0;k<n;k++){
-            order+=1;
-            if(array_1[j]==array_1[k]){
-                count+=1;
-                updated_array[order-1]=updated_array[j];
-            }
-            else{
-                updated_array[order-1]=updated_array[j];
-            }
-            printf("%d %d\n",updated_array[j],count);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(array_1[j]>array_1[j+1]){
+                array_1[j]=array_1[j+1];
+                temp=array_1[j];
+                array_1[j+1]=temp;
             }
         }
+    }
+    for(int x=0;x<n;x++){
+        for(int z=x;z<n-x;z++){
+            if(array_1[z]==array_1[z+1]){
+                frequency+=1;
+            }
+        }
+        printf("%d %d",array_1[x],frequency );
+    }
     return 0;
 }
